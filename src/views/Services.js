868643c2e8f2fafde7594b397ac3b9eb6d84af4e
boski <boskitechh@gmail.com>
2022-06-15 00:28:49 +0100

    Added services page

--- src/views/Services.js
+++ src/views/Services.js
@@ -3,6 +3,11 @@ import { styled } from "@mui/system"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import VaccinesIcon from '@mui/icons-material/Vaccines';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import MedicationLiquidIcon from '@mui/icons-material/MedicationLiquid';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import HealingIcon from '@mui/icons-material/Healing';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 
 const QuestionAnswers = () => {
 
@@ -35,6 +40,17 @@ const StyledItemHeader = styled(Grid)(({theme}) => ({
     fontSize: '36px'
 }))
 
+const StyledItemHeading = styled(Grid)(({theme}) => ({
+    textAlign:'center',
+    fontSize:'20px',
+    fontWeight:600
+}))
+
+const StyledItemText = styled(Grid)(({theme}) => ({
+    textAlign:'center',
+    fontSize:'19px',
+    marginTop:15
+}))
 
   return (
     <div>
@@ -47,33 +63,55 @@ const StyledItemHeader = styled(Grid)(({theme}) => ({
                     <Grid item md={4} xs={12}>
                         <StyledItemCard>
                             <StyledItemHeader>
-                                <VaccinesIcon/>
+                                <VaccinesIcon sx={{fontSize:'60px', color:'#07163a'}}/>
                             </StyledItemHeader>
+                            <StyledItemHeading>MEDICAL CHECK UP</StyledItemHeading>
+                            <StyledItemText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</StyledItemText>
                         </StyledItemCard>
                     </Grid>
                     <Grid item md={4} xs={12}>
                         <StyledItemCard>
-
+                            <StyledItemHeader>
+                                <MedicalServicesIcon sx={{fontSize:'60px', color:'#07163a'}}/>
+                            </StyledItemHeader>
+                            <StyledItemHeading>EMERGENCY</StyledItemHeading>
+                            <StyledItemText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</StyledItemText>
                         </StyledItemCard>
                     </Grid>
                     <Grid item md={4} xs={12}>
                         <StyledItemCard>
-
+                            <StyledItemHeader>
+                                <HealingIcon sx={{fontSize:'60px', color:'#07163a'}}/>
+                            </StyledItemHeader>
+                            <StyledItemHeading>DAMAGE CONTROL</StyledItemHeading>
+                            <StyledItemText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</StyledItemText>
                         </StyledItemCard>
                     </Grid>
                     <Grid item md={4} xs={12}>
                         <StyledItemCard>
-
+                            <StyledItemHeader>
+                                <MedicationLiquidIcon sx={{fontSize:'60px', color:'#07163a'}}/>
+                            </StyledItemHeader>
+                            <StyledItemHeading>FIRST AID</StyledItemHeading>
+                            <StyledItemText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</StyledItemText>
                         </StyledItemCard>
                     </Grid>
                     <Grid item md={4} xs={12}>
                         <StyledItemCard>
-
+                            <StyledItemHeader>
+                                <MonitorHeartIcon sx={{fontSize:'60px', color:'#07163a'}}/>
+                            </StyledItemHeader>
+                            <StyledItemHeading>HEART RADIOGRAPHY</StyledItemHeading>
+                            <StyledItemText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</StyledItemText>
                         </StyledItemCard>
                     </Grid>
                     <Grid item md={4} xs={12}>
                         <StyledItemCard>
-                            
+                            <StyledItemHeader>
+                                <PeopleOutlineIcon sx={{fontSize:'60px', color:'#07163a'}}/>
+                            </StyledItemHeader>
+                            <StyledItemHeading>THERAPY</StyledItemHeading>
+                            <StyledItemText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</StyledItemText>
                         </StyledItemCard>
                     </Grid>
                 </StyledBodyGrid>
