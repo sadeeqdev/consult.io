7fe823058ce0a5a73dd946997fe12f4ea429e5db
boski <boskitechh@gmail.com>
2022-06-15 17:02:45 +0100

    add pugs

--- src/views/Consultation.js
+++ src/views/Consultation.js
@@ -8,7 +8,7 @@ const Consultation = () => {
 
   const StyledBanner = styled('div')(({theme})=>({
     width: '100%',
-    height: 370,
+    height: 350,
     backgroundColor: '#07163a',
     color: 'white',
     textAlign:'center',
@@ -57,13 +57,6 @@ const Consultation = () => {
       }
   }))
 
-  const StyledTitleBlock = styled('div')(({theme}) => ({
-    marginTop:'100px',
-    fontSize:'35px',
-    fontWeight:600,
-    color:'#07163a'
-  }))
-
   const StyledBody = styled('div')(({theme}) => ({
       width:'60%',
       margin:'-150px auto auto auto',
@@ -76,10 +69,8 @@ const Consultation = () => {
   const StyledItemCard = styled(Grid)(({theme}) => ({
       width:'100%',
       backgroundColor:'#fff',
-      height:'670px',
+      height:'570px',
       boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)',
-      display:'flex',
-      justifyContent: 'center'
       }))
 
   const StyledItemHeading = styled(Grid)(({theme}) => ({
@@ -90,9 +81,6 @@ const Consultation = () => {
   }))
 
   const StyledItemText = styled(Grid)(({theme}) => ({
-      textAlign:'justify',
-      lineHeight:'30px',
-      fontSize:'18px',
       padding:'20px 30px 15px 30px'
   }))
 
@@ -101,11 +89,15 @@ const Consultation = () => {
       height:'45px',
       backgroundColor:'#07163a',
       color: '#fff',
+      bottom: 0,
       fontWeight: '900',
-      marginTop: '25px',
       '&:hover':{backgroundColor:'#2f53a5', color:'white'}
-
   })
+
+  const StyledHeaderText = styled('div')(({theme}) => ({
+      textAlign:'center',
+      fontSize:'16px',
+  }))
 
   return (
     <Fragment>
@@ -123,13 +115,24 @@ const Consultation = () => {
       <StyledBody>
         <StyledBodyGrid container spacing={2}>
             <Grid item md={4} xs={12}>
-                <StyledItemCard>
+            <StyledItemCard>
                     <div>
-                    <StyledItemHeading>One-time</StyledItemHeading>
+                    <StyledItemHeading>One-Time</StyledItemHeading>
                     <StyledItemText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        <StyledItemButton>
-                            Enroll
+                      <StyledHeaderText>
+                        <span style={{fontSize:'40px', fontWeight:'700'}}>$5</span>/session
+                      </StyledHeaderText>
+                      <ul style={{padding:15, fontSize:'20px', lineHeight:'40px', color:'#333'}}>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                      </ul>    
+                      <StyledItemButton>
+                            Select
                         </StyledItemButton>                          
                     </StyledItemText>
                     </div>
@@ -140,10 +143,22 @@ const Consultation = () => {
                     <div>
                     <StyledItemHeading>Monthly</StyledItemHeading>
                     <StyledItemText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        <StyledItemButton>
-                            Enroll
-                        </StyledItemButton>                          
+                      <StyledHeaderText>
+                        <span style={{fontSize:'40px', fontWeight:'700'}}>$30</span>/year
+                      </StyledHeaderText>
+                      <ul style={{padding:15, fontSize:'20px', lineHeight:'40px', color:'#333'}}>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                      </ul>    
+                      <StyledItemButton>
+                         Select
+                      </StyledItemButton>    
+                                          
                     </StyledItemText>
                     </div>
                 </StyledItemCard>
@@ -153,9 +168,20 @@ const Consultation = () => {
                     <div>
                     <StyledItemHeading>Yearly</StyledItemHeading>
                     <StyledItemText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        <StyledItemButton>
-                            Enroll
+                      <StyledHeaderText>
+                        <span style={{fontSize:'40px', fontWeight:'700'}}>$300</span>/year
+                      </StyledHeaderText>
+                      <ul style={{padding:15, fontSize:'20px', lineHeight:'40px', color:'#333'}}>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                      </ul>    
+                      <StyledItemButton>
+                            Select
                         </StyledItemButton>                          
                     </StyledItemText>
                     </div>
